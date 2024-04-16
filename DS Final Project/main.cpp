ea6cb995c307a8a655324aa6d73e88ee3393a2de
MuhammadElsoukkary <119015633+MuhammadElsoukkary@users.noreply.github.com>
2024-04-16 11:17:36 -0400

    Add project files.

---
+++ DS Final Project/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+    // Play the audio file using Windows Multimedia API
+    printf("Playing right now\n");
+    if (PlaySound(TEXT("C:\\Users\\muham\\OneDrive\\Desktop\\DS Final Project\\DS Final Project\\randomsound.wav"), NULL, SND_FILENAME) == 0) {
+        std::cerr << "Failed to play audio file" << std::endl;
+        return 1;
+    }
+
+    // Wait for the audio to finish playing
+    Sleep(3000); // Adjust the duration as needed
+
+    return 0;
+}
+
+
