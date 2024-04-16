96116e5e4276fab56aceb063b0005dce779be7a8
MuhammadElsoukkary <119015633+MuhammadElsoukkary@users.noreply.github.com>
2024-04-16 11:25:24 -0400

    Some quick notes

--- DS Final Project/main.cpp
+++ DS Final Project/main.cpp
@@ -3,6 +3,13 @@
 
 int main(void) {
     // Play the audio file using Windows Multimedia API
+
+
+    // this libarary appears to be the simplest libarary for playing audio 
+    // we may change it up but for now we stick this one
+    // all files MUST be .wav for them to play on it so keep that in mind
+
+
     printf("Playing right now\n");
     if (PlaySound(TEXT("C:\\Users\\muham\\OneDrive\\Desktop\\DS Final Project\\DS Final Project\\randomsound.wav"), NULL, SND_FILENAME) == 0) {
         std::cerr << "Failed to play audio file" << std::endl;
